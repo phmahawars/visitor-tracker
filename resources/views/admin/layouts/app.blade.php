0dfba5e972dc7d3c01d16ded8e7d33a00c427713
Harsh Kumar <phmahawars@gmai.com>
2025-05-10 18:52:43 +0530

    dffdw

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -22,15 +22,15 @@
     {{-- Navbar --}}
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('dashboard.index') }}">Visitor Tracker</a>
+            <a class="navbar-brand" href="{{ route('logs.index') }}">Visitor Tracker</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
+                    {{-- <li class="nav-item">
                         <a class="nav-link" href="{{ route('dashboard.index') }}">Dashboard</a>
-                    </li>
+                    </li> --}}
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('logs.index') }}">Visitor Logs</a>
                     </li>
