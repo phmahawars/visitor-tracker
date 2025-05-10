366ccd972a0af89b5b525c2067873e8b3f5c3f64
Harsh Kumar <phmahawars@gmai.com>
2025-05-10 18:51:30 +0530

    dffdw

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware([SetCustomUserAgent::class])->group(function () {
 
     // Dashboard
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+    // Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
 
     // Visitor Logs
-    Route::get('/logs', [VisitorController::class, 'logs'])->name('logs.index');
+    Route::get('/', [VisitorController::class, 'logs'])->name('logs.index');
     Route::get('/blocked-ips', [VisitorController::class, 'blockedIp'])->name('ips.blocked');
     Route::post('/block-ip', [VisitorController::class, 'blockIp'])->name('ips.block');
     Route::post('/unblock-ip', [VisitorController::class, 'unblockIp'])->name('ips.unblock');
